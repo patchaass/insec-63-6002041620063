49171ed27104a2adce4be1697716cce45bec4d36
patchaass <48791752+patchaass@users.noreply.github.com>
2020-10-22 18:24:52 +0700

    Create migration Create permission of post

--- console/migrations/m201022_104836_create_permission_of_post.php
+++ console/migrations/m201022_104836_create_permission_of_post.php
@@ -12,6 +12,26 @@ class m201022_104836_create_permission_of_post extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
+        $create=$auth->createPermission('post-create');
+        $create->description='Create a post';
+        $auth->add($create);
+
+        $index=$auth->createPermission('post-index');
+        $index->description='List a post';
+        $auth->add($index); 
+        
+        $update=$auth->createPermission('post-update');
+        $update->description='Update a post';
+        $auth->add($update);
+
+        $view=$auth->createPermission('post-view');
+        $view->description='View a post';
+        $auth->add($view); 
+        
+        $delete=$auth->createPermission('post-delete');
+        $delete->description='Delete a post';
+        $auth->add($delete);
 
     }
 
@@ -20,9 +40,30 @@ class m201022_104836_create_permission_of_post extends Migration
      */
     public function safeDown()
     {
-        echo "m201022_104836_create_permission_of_post cannot be reverted.\n";
-
-        return false;
+        //echo "m201022_104836_create_permission_of_post cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
+        
+        $create=$auth->getRole('post-create');
+        if ($create){
+            $auth->remove($create);
+        }
+        $index=$auth->getRole('post-index');
+            if ($index){
+                $auth->remove($index);
+            }
+        $update=$auth->getRole('update-post');
+            if ($update){
+                $auth->remove($update);
+            }
+        $view=$auth->getRole('view-post');
+            if ($view){
+                $auth->remove($view);
+            }
+        $delete=$auth->getRole('delete-post');
+        if ($delete){
+            $auth->remove($delete);
+        }
+        return true;
     }
 
     /*
--- console/migrations/m201022_110846_create_item_for_rbac.php
+++ console/migrations/m201022_110846_create_item_for_rbac.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m201022_093631_create_item_for_rbac
+ * Class m201022_110846_create_item_for_rbac
  */
-class m201022_093631_create_item_for_rbac extends Migration
+class m201022_110846_create_item_for_rbac extends Migration
 {
     /**
      * {@inheritdoc}
@@ -13,21 +13,15 @@ class m201022_093631_create_item_for_rbac extends Migration
     public function safeUp()
     {
         $auth = Yii::$app->authManager;
-        
-        $admin=$auth->createRole('admin');
+        $admin = $auth->createRole('admin');
         $admin->description = 'Admin';
         $auth->add($admin);
-
         $author=$auth->createRole('author');
         $author->description = 'Author';
         $auth->add($author);
-
         $superAdmin=$auth->createRole('super-admin');
         $superAdmin->description = 'Super Admin';
         $auth->add($superAdmin);
-        
-        //print_r($auth);
-        //return false;
     }
 
     /**
@@ -35,9 +29,8 @@ class m201022_093631_create_item_for_rbac extends Migration
      */
     public function safeDown()
     {
-        //echo "m201022_093631_create_item_for_rbac cannot be reverted.\n";
+        //echo "m201022_110846_create_item_for_rbac cannot be reverted.\n";
         $auth = Yii::$app->authManager;
-
         $admin=$auth->getRole('admin');
             if ($admin){
                 $auth->remove($admin);
@@ -50,7 +43,6 @@ class m201022_093631_create_item_for_rbac extends Migration
             if ($superAdmin){
                 $auth->remove($superAdmin);
             }
-
         return true;
     }
 
@@ -63,7 +55,7 @@ class m201022_093631_create_item_for_rbac extends Migration
 
     public function down()
     {
-        echo "m201022_093631_create_item_for_rbac cannot be reverted.\n";
+        echo "m201022_110846_create_item_for_rbac cannot be reverted.\n";
 
         return false;
     }
