f321c9793f32a973fac36426a1e059a51dfa4f83
patchaass <48791752+patchaass@users.noreply.github.com>
2020-10-22 17:50:11 +0700

    create migration for role

---
+++ console/migrations/m201022_104836_create_permission_of_post.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201022_104836_create_permission_of_post
+ */
+class m201022_104836_create_permission_of_post extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m201022_104836_create_permission_of_post cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m201022_104836_create_permission_of_post cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
